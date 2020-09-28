0acdde28cc5b640928ada6e87bd205090b614c7a
Victor Forbes <vhforbes@gmail.com>
2020-09-28 14:10:35 -0300

    Importing function from myName.js

--- First-WebPack/src/index.js
+++ First-WebPack/src/index.js
@@ -1,9 +1,10 @@
 import _ from 'lodash'
+import myName from './myName'
 
 function component() {
   const element = document.createElement('div')
 
-  element.innerHTML = _.join(['Hello, webpack scrip', ' '])
+  element.innerHTML = myName('Victor')
 
   return element
 }
